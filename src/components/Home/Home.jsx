85123143c63eb6e73eb04c64e5151fbd692dea7e
emayethossen <samir2021hossen@gmail.com>
2023-05-03 04:44:15 +0600

    slider sec added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,9 @@ import ggg from '../../assets/img3.jpg'
 import ChefCard from '../ChefCard/ChefCard';
 import Recipe from '../Recipe/Recipe';
 import SugarRec from '../SugarRes/SugarRec';
+import Slider from '../Slider/Slider';
+
+
 const Home = () => {
 
     const [chefs, setChefs] = useState([])
@@ -17,7 +20,7 @@ const Home = () => {
 
     return (
         <div className='w-full'>
-            <div style={{ backgroundImage: `url(${ggg})`, backgroundSize: 'cover', height: '100vh' }} className='grid grid-cols-2 gap-5 bg-no-repeat'>
+            <div style={{ backgroundImage: `url(${ggg})`, backgroundSize: 'cover', height: '100vh' }} className='lg:grid grid-cols-2 gap-5 bg-no-repeat'>
                 <div>
                     <h2 className='text-5xl font-bold'>Let's Start Cooking With Popular Recipes</h2>
                     <p>Want to learn cook but confused, how to start? <br /> No need to worry again!!!</p>
@@ -25,38 +28,10 @@ const Home = () => {
                     <button className="btn btn-error">Explore More</button>
                 </div>
                 <div>
-                    <div className="carousel w-full">
-                        <div id="slide1" className="carousel-item relative w-full">
-                            <img src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide4" className="btn btn-circle">❮</a>
-                                <a href="#slide2" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                        <div id="slide2" className="carousel-item relative w-full">
-                            <img src={ggg} className="w-full" />
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide1" className="btn btn-circle">❮</a>
-                                <a href="#slide3" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                        <div id="slide3" className="carousel-item relative w-full">
-                            <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide2" className="btn btn-circle">❮</a>
-                                <a href="#slide4" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                        <div id="slide4" className="carousel-item relative w-full">
-                            <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide3" className="btn btn-circle">❮</a>
-                                <a href="#slide1" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
+                <Slider />
+            
             <div className='w-10/12 mx-auto'>
                 <h1 className='text-center text-3xl font-bold my-10'>
                     All Chef
---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+
+import { EffectCoverflow, Pagination, Navigation } from 'swiper';
+
+import img1 from '../../assets/low-sugar-images/sug1.jpg'
+import img2 from '../../assets/low-sugar-images/sug2.jpg'
+import img3 from '../../assets/low-sugar-images/sug3.jpg'
+import img4 from '../../assets/low-sugar-images/sug4jpg.jpg'
+import img5 from '../../assets/low-sugar-images/sug5.jpg'
+import img6 from '../../assets/low-sugar-images/sug6.jpg'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+
+const Slider = () => {
+    return (
+        <div className='container'>
+            <h1 className='heading'>Trending Food</h1>
+            <Swiper
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                loop={true}
+                spaceBetween={0}
+                slidesPerView={'auto'}
+                pagination={{ el: '.swiper-pagination', clickable: true }}
+                navigation={{
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                    clickable:true
+                }}
+                modules={[EffectCoverflow, Pagination, Navigation]}
+                coverflowEffect={
+                    {
+                        rotate: 0,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 2.5
+                    }
+                }
+                className='swiper_container'
+            >
+                <SwiperSlide>
+                    <img src={img1} alt="slide_image" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img2} alt="slide_image" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img3} alt="slide_image" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img4} alt="slide_image" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img5} alt="slide_image" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img6} alt="slide_image" />
+                </SwiperSlide>
+
+                <div className="slider-controler">
+                    <div className="swiper-button-prev slider-arrow">
+                        <FaArrowLeft className='arrow-back-outline' />
+                    </div>
+                    <div className="swiper-button-next slider-arrow">
+                        <FaArrowRight className='arrow-forward-outline' />
+                    </div>
+                    <div className="swiper-pagination">
+
+                    </div>
+                </div>
+
+            </Swiper>
+        </div>
+    );
+};
+
+export default Slider;
