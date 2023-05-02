63cbf990fe21bad150a1b4f23ffeafe756f7ede6
emayethossen <samir2021hossen@gmail.com>
2023-05-02 07:45:49 +0600

    react toast added

--- src/components/RecipeItems/RecipeItems.jsx
+++ src/components/RecipeItems/RecipeItems.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const RecipeItems = ({ recipe }) => {
 
     const { name, image, price, rating } = recipe
-
+    const notify = () => toast("Your Favourite Recipe Added !");
     return (
         <div>
             <div className="card card-compact bg-base-100 shadow-xl">
@@ -16,7 +18,8 @@ const RecipeItems = ({ recipe }) => {
                     </div>
                     <div className="card-actions justify-center">
                         <button className="btn btn-primary">Order Now</button>
-                        <button className="btn btn-primary">Favourite</button>
+                        <button onClick={notify} className="btn btn-primary">Favourite</button>
+                        <ToastContainer />
                     </div>
                 </div>
             </div>
